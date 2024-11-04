3c72fc4acb564016d0e50f1b9664d93c8978c357
sSANATGAR <s.sanatgar@dontenwill.de>
2024-11-04 22:58:40 +0100

    [FEATURE] Refactor configuration handling and EEPROM integration

--- src/HelperUtils.cpp
+++ src/HelperUtils.cpp
@@ -31,3 +31,79 @@ String HelperUtils::getMacAddress()
     }
     return HelperUtils::toUpperCase(macStr);
 }
+
+void HelperUtils::initEEPROM(Config &config)
+{
+    EEPROM.begin(sizeof(Config));
+}
+
+void HelperUtils::saveConfigToEEPROM(Config &config)
+{
+    config.version = CONFIG_VERSION;
+    EEPROM.put(CONFIG_START_ADDRESS, config);
+    EEPROM.commit();
+    Serial.println("Konfiguration wurde gespeichert.");
+}
+
+bool HelperUtils::loadConfigFromEEPROM(Config &config)
+{
+    EEPROM.get(CONFIG_START_ADDRESS, config);
+    if (config.version != CONFIG_VERSION)
+    {
+        return false; 
+    }
+    return true;
+}
+
+void HelperUtils::parseConfigString(String &inputString, Config &config) {
+  // String anhand von ';' aufteilen
+  int start = 0;
+  while (start < inputString.length()) {
+    int end = inputString.indexOf(';', start);
+    if (end == -1) {
+      end = inputString.length();
+    }
+    String token = inputString.substring(start, end);
+    // Token verarbeiten, sollte in der Form key="value" oder key=value sein
+    int eqIndex = token.indexOf('=');
+    if (eqIndex != -1) {
+      String key = token.substring(0, eqIndex);
+      String value = token.substring(eqIndex + 1);
+      value.trim();
+      if (value.startsWith("\"") && value.endsWith("\"")) {
+        value = value.substring(1, value.length() - 1);
+      }
+      key.trim();
+      if (key == "apn") {
+        value.toCharArray(config.apn, sizeof(config.apn));
+      } else if (key == "gprsUser") {
+        value.toCharArray(config.gprsUser, sizeof(config.gprsUser));
+      } else if (key == "gprsPass") {
+        value.toCharArray(config.gprsPass, sizeof(config.gprsPass));
+      } else if (key == "GSM_PIN") {
+        value.toCharArray(config.GSM_PIN, sizeof(config.GSM_PIN));
+      } else if (key == "server") {
+        value.toCharArray(config.server, sizeof(config.server));
+      } else if (key == "port") {
+        config.port = value.toInt();
+      } else if (key == "username") {
+        value.toCharArray(config.username, sizeof(config.username));
+      } else if (key == "password") {
+        value.toCharArray(config.password, sizeof(config.password));
+      } else {
+        Serial.print("Unbekannter Schlüssel: ");
+        Serial.println(key);
+      }
+    }
+    start = end + 1;
+  }
+}
+
+// Funktion zum Zurücksetzen des EEPROM
+void HelperUtils::resetEEPROM() {
+  Config emptyConfig;
+  emptyConfig.version = 0xFF; 
+  EEPROM.put(CONFIG_START_ADDRESS, emptyConfig);
+  EEPROM.commit();
+  Serial.println("EEPROM wurde zurückgesetzt.");
+}
