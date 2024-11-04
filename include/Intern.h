81e270d15c30c14d8625bc288dc7a1e278b25045
sSANATGAR <s.sanatgar@dontenwill.de>
2024-11-04 23:16:25 +0100

    [FEATURE] Update firmware version to 0.3.0

--- include/Intern.h
+++ include/Intern.h
@@ -2,7 +2,7 @@
 #ifndef Intern_H
 #define Intern_H
 
-#define FIRMWARE_VERSION "0.2.0"
+#define FIRMWARE_VERSION "0.3.0"
 #define RFID_FILE_NAME "/rfids.txt"
 #define FIRMWARE_FILE_NAME "/firmware.bin"
 #define LOG_FILE_NAME "/logs.json"
