e5943ced2c6c9f626c07a150ac06c67038d1bd7f
finn <finndruenert@gmail.com>
2025-09-14 15:44:41 +0200

    bf

--- src/Modem.cpp
+++ src/Modem.cpp
@@ -476,8 +476,7 @@ uint64_t Modem::getUnixTimestamp()
 
 esp_err_t Modem::increaseWatchdogTimeoutForFileUpload(const size_t fileSize)
 {
-    constexpr uint32_t uploadSpeed = 9500; // [B/s] TODO: measure speed
-    const uint32_t uploadTime = fileSize / uploadSpeed; // [s]
+    const uint32_t uploadTime = fileSize / estimatedUploadSpeed; // [s]
     if (uploadTime <= HW_WATCHDOG_DEFAULT_TIMEOUT / 5) return ESP_OK; // Doesnt really matter
     const uint32_t newWatchdogTime = uploadTime + HW_WATCHDOG_DEFAULT_TIMEOUT; // [s]
     return WatchdogHandler::setTempTimeout(newWatchdogTime);
