81436ade6da22acf430c4f1111d5b2cff47966e7
Finn Drünert <finndruenert@gmail.com>
2025-11-18 21:34:37 +0100

    remove unused define

--- include/Intern.h
+++ include/Intern.h
@@ -8,7 +8,6 @@
 #define LOG_FILE_PATH "/log.log"
 #define GPS_FILE_PATH "/gps.bin"
 #define GPS_TRACKING_CONSENTED_RFIDS_FILE_PATH "/rfids_gps_consent.bin"
-#define CONNECTION_SPEED_TEST_FILE_PATH "/speed_test.bin"
 
 #define CONFIG_VERSION "1"
 #define CONFIG_PREFS_NAME "Config v" CONFIG_VERSION
