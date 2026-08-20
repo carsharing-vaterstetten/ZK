4bfbd0ef9b120dd1f7a81d93df816eade7e1f872
Finn Drünert <finndruenert@gmail.com>
2026-08-20 10:06:14 +0200

    build with hw rev flag

--- src/config/Intern.h
+++ src/config/Intern.h
@@ -1,6 +1,18 @@
 #pragma once
 
-#define FIRMWARE_VERSION "2.0.0-main"
+#ifdef HW_REV
+#if HW_REV == 3
+#define FIRMWARE_VERSION_FLAG "hw3"
+#elif HW_REV == 2
+#define FIRMWARE_VERSION_FLAG "hw2"
+#else
+#error "Invalid HW_REV"
+#endif
+#else
+#define FIRMWARE_VERSION_FLAG "main"
+#endif
+
+#define FIRMWARE_VERSION "2.0.0-" FIRMWARE_VERSION_FLAG
 
 #define CONFIG_VERSION "1"
 #define CONFIG_PREFS_NAME "Config v" CONFIG_VERSION
